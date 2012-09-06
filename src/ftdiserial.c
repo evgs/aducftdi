0008e84042a0030d16643213d6c6cf3674a7cbf4
Eugene Stahov <evgs@technom.ru>
2012-09-06 16:07:09 +0600

    ftdi serial read/write

--- src/ftdiserial.c
+++ src/ftdiserial.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <ftdi.h>
 
 #include "serial.h"
 
@@ -120,6 +119,11 @@ int bindFtdi(const char *addr) {
   
 }
 
+int setFtdiBaudRate(int baudrate) {
+  ftdi_set_line_property(fc, BITS_8, STOP_BIT_1, NONE);
+  return ftdi_set_baudrate(fc, baudrate);
+}
+
 int printFtdiInfo() {
       unsigned char eebuf[FTDI_DEFAULT_EEPROM_SIZE];
       ftdi_read_eeprom(fc, eebuf);
@@ -159,83 +163,39 @@ int aducFtdiReset(int pm) {
     ftdi_disable_bitbang(fc);
 }
   
+#include "timeout.h"
 
 int serial_read_fully(uint8_t *buffer, int count, int timeout)
 {
-/*	struct pollfd fds;
-	int rc;
-	int len;
-	uint8_t *ptr = buffer;
-
-	while (count > 0) {
-		fds.fd = fd;
-		fds.events = POLLIN;
-
-		rc = poll(&fds, 1, timeout);
-		if (rc == 0) {
-			fprintf(stderr, "serial_read_fully: poll timeout\n");
-			break;
-		} else if (rc < 0) {
-			fprintf(stderr, "serial_read_fully: poll failed %s\n", strerror(errno));
-			return -1;
-		}
-
-		if (fds.revents & (POLLERR | POLLHUP | POLLNVAL)) {
-			fprintf(stderr, "serial_read_fully: poll revents (%hx)\n", fds.revents);
-			return -1;
-		} else if (fds.revents & (POLLIN)) {
-			len = read(fd, ptr, count);
-			if (len < 0) {
-				fprintf(stderr, "serial_read_fully: read failed %s\n", strerror(errno));
-				return -1;
-			}
-			ptr += len;
-			count -= len;
-		}
-	}
-
-	return ptr - buffer;
-*/
+  uint8_t *ptr = buffer;
   
+  setTimeout(timeout);
+
+  while (count>0) {
+    int r=ftdi_read_data(fc, ptr, count);
+    
+    count-=r; ptr+=r;
+    if (isTimeout()) break;
+  }
+
+  cancelTimeout();
+  return ptr - buffer;
 }
 
 int serial_write_fully(const uint8_t *buffer, int count, int timeout)
 {
-/*	struct pollfd fds;
-	int rc;
-	int len;
-	const uint8_t *ptr = buffer;
+  uint8_t const *ptr = buffer;
+  setTimeout(timeout);
 
-	while (count > 0) {
-		fds.fd = fd;
-		fds.events = POLLOUT;
-
-		rc = poll(&fds, 1, timeout);
-		if (rc == 0) {
-			fprintf(stderr, "serial_write_fully: poll timeout\n");
-			break;
-		} else if (rc < 0) {
-			fprintf(stderr, "serial_write_fully: poll failed %s\n", strerror(errno));
-			return -1;
-		}
-
-		if (fds.revents & (POLLERR | POLLHUP | POLLNVAL)) {
-			fprintf(stderr, "serial_write_fully: poll revents (%hx)\n", fds.revents);
-			return -1;
-		} else if (fds.revents & (POLLOUT)) {
-			len = write(fd, ptr, count);
-			if (len < 0) {
-				fprintf(stderr, "serial_write_fully: write failed %s\n", strerror(errno));
-				return -1;
-			}
-			ptr += len;
-			count -= len;
-		}
-	}
-
-	return ptr - buffer;
-*/
+  while (count>0) {
+    int w=ftdi_write_data(fc, ptr, count);
+    
+    count-=w; ptr+=w;
+    if (isTimeout()) break;
+  }
   
+  cancelTimeout();
+  return ptr - buffer;
 }
 
 static const ReadWrite ftdiRW = {
